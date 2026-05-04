40b9fd7c3ac783d93d714ac8c318d43886eb4bb9
valdyoz <25051204048@mhs.unesa.ac.id>
2026-05-04 21:24:48 +0700

    Change data source and update comments
    Updated data source from 'Book1.xlsx' to 'data.csv' and adjusted related comments.

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Dashboard Penjualan", layout="wide")
 # =============================================
 @st.cache_data
 def load_data():
-    df = pd.read_excel("Book1.xlsx")
+    df = pd.read_excel("data.csv")
 
     # Bersihkan nama kolom
     new_columns = []
